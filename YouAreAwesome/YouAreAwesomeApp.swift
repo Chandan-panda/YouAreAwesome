33f2af55ba412af5453e79ff1db1a6ecd979e9e0
Chandan Panda <81402425+Chandan-panda@users.noreply.github.com>
2025-04-29 22:28:04 +0530

    Initial Commit

---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by Chandan Panda on 29/04/25.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
