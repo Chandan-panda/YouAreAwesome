06f29f3680daad90751911eee5939f611309f116
Chandan Panda <81402425+Chandan-panda@users.noreply.github.com>
2025-04-30 23:00:41 +0530

    Ch 1.3 Completed

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,9 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var message="Football"
+    @State private var message=""
     var body: some View {
         VStack {
+            Spacer()
             Text("What is Football to You ?")
                 .font(.largeTitle)
                 .fontWeight(.light)
@@ -20,23 +21,48 @@ struct ContentView: View {
                     .resizable()
                     .scaledToFit()
                     .foregroundStyle(.blue)
+                    .frame(width: 100, height: 100)
+                Spacer()
                 Image(systemName: "figure.australian.football")
                     .resizable()
                     .scaledToFit()
                     .foregroundStyle(.indigo)
+                    .frame(width: 100, height: 100)
+                Spacer()
                 Image(systemName: "figure.indoor.soccer")
                     .resizable()
                     .scaledToFit()
                     .foregroundStyle(.purple)
+                    .frame(width: 100, height: 100)
             }
-            
-            Button("Click"){
-                message="It is Soccer !!!"
-            }
+            Spacer()
             Text(message)
-                .font(.largeTitle)
+                .font(.title2)
                 .fontWeight(.bold)
                 .foregroundStyle(.red)
+            
+            HStack{
+                Button("Football"){
+                    message="Football"
+                }
+                
+                Button("Soccer"){
+                    message="Soccer"
+                }
+            }
+            .buttonStyle(.borderedProminent)
+            .tint(.green)
+            
+//            Button("Click"){
+//                message="It is Soccer !!!"
+//            }
+//            .buttonStyle(.borderedProminent)
+//            .font(.title)
+//            .tint(.green)
+//            Text(message)
+//                .font(.largeTitle)
+//                .fontWeight(.bold)
+//                .foregroundStyle(.red)
         }
         .padding()
     }
